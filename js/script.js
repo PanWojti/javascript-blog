3a0df2c2c7e21e0b19ccec8ee711edbbb92a5f26
Wojciech Sitkiewicz <sitkiewicz.wojciech@gmail.com>
2019-08-14 20:30:23 +0200

    Generate tags

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
   /* [DONE] remove class 'active' from all articles */
@@ -41,7 +41,8 @@ const titleClickHandler = function(event){
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 const generateTitleLinks = function () {
   const clickedElement = this;
@@ -95,3 +96,35 @@ const authors = document.querySelectorAll('.authors a');
 for(let author of authors){
   author.addEventListener('click', generateTitleLinks);
 }
+
+function generateTags(){
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+    /* find tags wrapper */
+    const tagsList = article.querySelector(optArticleTagsSelector);
+    /* make html variable with empty string */
+    let html = ' ';
+    /* get tags from data-tags attribute */
+    const dataTags = article.getAttribute('data-tags');
+    console.log(dataTags);
+    /* split tags into array */
+    const dataTagsArray = dataTags.split(' ');
+    console.log(dataTagsArray);
+    /* START LOOP: for each tag */
+    for (let tag of dataTagsArray) {
+      /* generate HTML of the link */
+      html = html + '<li><a href="#">'+tag+'</a></li>';
+      console.log(html);
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+    }
+    /* insert HTML of all the links into the tags wrapper */
+    article.innerHTML = html;
+  /* END LOOP: for every article: */
+  }
+}
+
+generateTags();
